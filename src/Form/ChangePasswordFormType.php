972965fac5be5829ed8b247b8b0672e494017743
zaiterassia <assia-z@live.fr>
2023-06-08 15:36:11 +0200

    correct reset password form

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -18,7 +18,8 @@ class ChangePasswordFormType extends AbstractType
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => [
-                    'attr' => ['autocomplete' => 'new-password'],
+                    'attr' => ['autocomplete' => 'new-password', 'class' => 'input-material'],
+                    'label_attr' => ['class' => 'label-material'],
                     'constraints' => [
                         new NotBlank([
                             'message' => 'Please enter a password',
@@ -30,11 +31,12 @@ class ChangePasswordFormType extends AbstractType
                             'max' => 4096,
                         ]),
                     ],
-                    'label' => 'New password',
+                    'label' => 'Nouveau mot de passe:',
                 ],
                 'second_options' => [
-                    'attr' => ['autocomplete' => 'new-password'],
-                    'label' => 'Repeat Password',
+                    'attr' => ['autocomplete' => 'new-password', 'class' => 'input-material'],
+                    'label_attr' => ['class' => 'label-material'],
+                    'label' => 'Confirmer votre mot de passe:',
                 ],
                 'invalid_message' => 'The password fields must match.',
                 // Instead of being set onto the object directly,
