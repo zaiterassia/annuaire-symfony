6e9fdb731a8a081411c52195ad9c5002d6b295e7
yracerlyn <yessicerlyn@gmail.com>
2023-06-08 11:29:38 +0200

    reset password

--- public/js/icon-eyes.js
+++ public/js/icon-eyes.js
@@ -2,18 +2,21 @@ $(document).ready(function () {
 
 const eye = document.querySelector(".feather-eye");
 const eyeoff = document.querySelector(".feather-eye-off");
-const passwordField = document.querySelector("input[type=password]");
+const passwordField = document.querySelector("[id='password']");
+const passwordField2 = document.querySelector("[id='password2']");
 
 eye.addEventListener("click", () => {
     eye.style.display = "none";
     eyeoff.style.display = "block";
     passwordField.type = "text";
+    passwordField2.type = "text";
   });
   
   eyeoff.addEventListener("click", () => {
     eyeoff.style.display = "none";
     eye.style.display = "block";
     passwordField.type = "password";
+    passwordField2.type = "password";
   });
 
 });
